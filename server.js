557ac238f21997980137abd6ce16321ac93164c9
chayan <developer@iwebgenics.com>
2025-03-19 15:55:14 +0530

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const passkeyRoutes = require('./routes/passkeys');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: [ 'https://admin.imailer.in/']
+  origin: [ 'https://admin.imailer.in/'],
+  credentials: true, // Allows cookies/tokens
+
 }));
 
 
